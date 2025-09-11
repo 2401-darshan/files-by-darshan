e4774f01a460fc40e9fdb20b459b52075cceabbd
Darshan Patel <darshankumar5619@gmail.com>
2025-09-11 10:36:10 +0530

    Update p1.c

--- DS/lab 20/p1.c
+++ DS/lab 20/p1.c
@@ -1,65 +1,101 @@
-#include <stdio.h>
+#include<stdio.h>
 
-void init() {
+int n;
+int hashset[100];
+
+void init()
+{
     for (int i = 0; i < n; i++)
     {
         hashset[i] = -1;
     }
 }
-int f(int key) {
+
+int f(int key)
+{
     return key % n;
 }
 
-void add(int key) {
+void add(int key)
+{
     int index = f(key);
-    while (hashset[index] != -1)
+    if (hashset[index] == -1)
     {
-        if (hashset[index] == key)
+        hashset[index] = key;
+        return;
+    }
+    if (hashset[index] == key)
+    {
+        printf("Key already exists\n");
+        return;
+    }
+    // collision resolution by linear probing
+    for (int probe = 1; probe < n; probe++)
+    {
+        int i = (index + probe) % n;
+        if (hashset[i] == -1)
         {
-            hashset[index] = key;
+            hashset[i] = key;
             return;
         }
-        else if (hashset[index] == key)
+        else if (hashset[i] == key)
         {
             printf("Key already exists\n");
             return;
         }
-        else
-        {
-            // collision
-            for (int probe = 1; probe < n; probe++)
-            {
-                int i = (index + probe) % n;
-                if (hashset[i] == -1)
-                {
-                    hashset[i] = key;
-                    return;
-                }
-                else if (hashset[i] == key)
-                {
-                    printf("Key already exists\n");
-                    return;
-                }
-            }
-            printf("Hashset is full\n");
-        }
     }
+    printf("Hashset is full\n");
 }
 
-void display() {
-    for(int i = 0; i < n; i++) {
+int contains(int key)
+{
+    int index = f(key);
+    for (int probe = 0; probe < n; probe++)
+    {
+        int i = (index + probe) % n;
+        if (hashset[i] == -1)
+        {
+            return 0;
+        }
+        if (hashset[i] == key)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void display()
+{
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", hashset[i]);
     }
     printf("\n");
 }
 
-int main() {
-    int i, key, n, hashset[100];
-    for(int i=0; i < n; i++) {
+int main()
+{
+    int key, count;
+    printf("Enter size of hashset: ");
+    scanf("%d", &n);
+    init();
+    printf("Enter number of elements to insert: ");
+    scanf("%d", &count);
+    
+    for (int i = 0; i < count; i++) {
         printf("Enter key: ");
         scanf("%d", &key);
         add(key);
     }
     display();
+    printf("Enter key to search: ");
+    scanf("%d", &key);
+    if (contains(key)) {
+        printf("Key found in hashset\n");
+    }
+    else {
+        printf("Key not found in hashset\n");
+    }
     return 0;
 }
