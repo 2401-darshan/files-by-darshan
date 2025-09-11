f2e7f7b500540e4c840fbae14b6507a0b94d53de
Darshan Patel <darshankumar5619@gmail.com>
2025-09-11 10:19:26 +0530

    Update p1.c

--- DS/lab 20/p1.c
+++ DS/lab 20/p1.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
-void init()
-{
+void init() {
     for (int i = 0; i < n; i++)
     {
         hashset[i] = -1;
     }
 }
-int f(int key)
-{
+int f(int key) {
     return key % n;
 }
-void add(int key)
-{
-    int idx = f(key);
+
+void add(int key) {
+    int index = f(key);
     while (hashset[index] != -1)
     {
         if (hashset[index] == key)
@@ -29,20 +27,39 @@ void add(int key)
         else
         {
             // collision
-            for (int probe = 0; probe < n; probe++)
+            for (int probe = 1; probe < n; probe++)
             {
-                int i = newIndex = (idx + probe) % n;
-                if (hashset[newIndex] == -1)
+                int i = (index + probe) % n;
+                if (hashset[i] == -1)
                 {
-                    hashset[newIndex] = key;
+                    hashset[i] = key;
                     return;
                 }
-                else if (hashset[newIndex] == key)
+                else if (hashset[i] == key)
                 {
                     printf("Key already exists\n");
                     return;
                 }
             }
+            printf("Hashset is full\n");
         }
     }
+}
+
+void display() {
+    for(int i = 0; i < n; i++) {
+        printf("%d ", hashset[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int i, key, n, hashset[100];
+    for(int i=0; i < n; i++) {
+        printf("Enter key: ");
+        scanf("%d", &key);
+        add(key);
+    }
+    display();
+    return 0;
 }
